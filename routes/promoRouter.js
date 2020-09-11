9413d4180f88bc6b83c80c9a29f3f36868b51da0
DonRuan22 <ruan_donino@hotmail.com>
2020-09-11 11:34:04 -0300

    Rest API with MongoDB 1

---
+++ routes/promoRouter.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const promoRouter = express.Router();
+ promoRouter.use(bodyParser.json());
+ promoRouter.route('/')
+.all((req,res,next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get((req,res,next) => {
+    res.end('Will send all the promotiones to you!');
+})
+.post((req, res, next) => {
+    res.end('Will add the promotion: ' + req.body.name + ' with details: ' + req.body.description);
+})
+.put((req, res, next) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on  promotiones');
+})
+.delete((req, res, next) => {
+    res.end('Deleting all promotiones');
+});
+
+
+promoRouter.route('/:promotionId')
+.all((req,res,next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get((req,res,next) => {
+    res.end('Will send all the promotiones to you!');
+})
+.post((req, res, next) => {
+    res.end('Will add the promotion: ' + req.body.name + ' with details: ' + req.body.description);
+})
+.put((req, res, next) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on  promotiones');
+})
+.delete((req, res, next) => {
+    res.end('Deleting all promotiones');
+});
+
+module.exports = promoRouter;
